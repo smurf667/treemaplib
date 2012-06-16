017a1c959a46bb87c9f8fa06c9a0252130b3ac96
jan@engehausen.de <jan@engehausen.de@8547119c-143f-a580-364c-7e1e9f52c241>
2012-06-16 13:16:06 +0000

    bump version due to endless loop on drilling down on a leaf

--- trunk/TreeMapBuild/swing_fileviewer.bat
+++ trunk/TreeMapBuild/swing_fileviewer.bat
@@ -1,2 +1,2 @@
 @echo off
-java -cp build/tm_swing_examples_0.0.3.jar;build/tm_core_0.0.3.jar;build/tm_swing_0.0.3.jar;build/tm_core_examples_0.0.3.jar de.engehausen.treemap.examples.swing.FileViewer
+java -cp build/tm_swing_examples_0.0.4.jar;build/tm_core_0.0.4.jar;build/tm_swing_0.0.4.jar;build/tm_core_examples_0.0.4.jar de.engehausen.treemap.examples.swing.FileViewer
