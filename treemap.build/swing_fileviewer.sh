0a9a99616c808b9765ab6acc6d602584bf790f06
smurf667@gmail.com <smurf667@gmail.com@8547119c-143f-a580-364c-7e1e9f52c241>
2015-01-11 18:10:56 +0000

    mavenize

--- treemap.build/swing_fileviewer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-. config.properties
-java -cp build/tm_swing_examples_${tm.version}.jar;build/tm_core_${tm.version}.jar;build/tm_swing_${tm.version}.jar;build/tm_core_examples_${tm.version}.jar de.engehausen.treemap.examples.swing.FileViewer
