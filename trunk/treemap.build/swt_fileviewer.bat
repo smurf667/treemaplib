41f3182bf2087c33b62f18fa2db077496a7b36ae
smurf667@gmail.com <smurf667@gmail.com@8547119c-143f-a580-364c-7e1e9f52c241>
2015-01-11 18:10:56 +0000

    mavenize

--- trunk/treemap.build/swt_fileviewer.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-java -cp ../TreeMapSWT/lib/swt.jar;build/tm_swt_examples_1.0.0.jar;build/tm_core_1.0.0.jar;build/tm_swt_1.0.0.jar;build/tm_core_examples_1.0.0.jar de.engehausen.treemap.examples.swt.FileViewer
--- trunk/treemap.build/swt_fileviewer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-. config.properties
-java -cp ../TreeMapSWT/lib/swt.jar;build/tm_swt_examples_${tm.version}.jar;build/tm_core_${tm.version}.jar;build/tm_swt_${tm.version}.jar;build/tm_core_examples_${tm.version}.jar de.engehausen.treemap.examples.swing.FileViewer
